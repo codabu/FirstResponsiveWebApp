94b89e0faed06f780c708e6da9b54a02695d786a
codabu <57690093+codabu@users.noreply.github.com>
2021-02-04 20:01:57 -0600

    Commit files to ghub

---
+++ FirstResponsiveWebAppBurton/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using FirstResponsiveWebAppBurton.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstResponsiveWebAppBurton.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AgeCalculatorModel age)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Age = age.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.Age = "";
+            }
+
+            return View();
+        }
+    }
+}
+
---
+++ FirstResponsiveWebAppBurton/Models/AgeCalculatorModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppBurton.Models
+{
+    public class AgeCalculatorModel
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        [RegularExpression("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$", ErrorMessage =
+            "Please enter a valid name")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your birth year")]
+        [Range(1900, 2015, ErrorMessage =
+            "Birth year must be between 1900 and 2015")]
+        public int? Year{ get; set; }
+
+        [Required(ErrorMessage = "Please enter your birth month")]
+        [Range(1, 12, ErrorMessage =
+            "Birth month must be between 1 and 12")]
+        public int? Month { get; set; }
+
+        [Required(ErrorMessage = "Please enter the day you were born")]
+        [Range(1, 31, ErrorMessage =
+            "Day of birth must be between 1 and 31")]
+        public int? Day { get; set; }
+        public string AgeThisYear()
+        {   string output;
+            DateTime dateToday = DateTime.Now;
+            int? todayDay = int.Parse(DateTime.Now.ToString("dd"));
+            int? todayMonth = int.Parse(DateTime.Now.ToString("MM"));
+            int? todayYear = int.Parse(DateTime.Now.ToString("yyyy"));
+            int? age;
+            age = todayYear - Year;
+            if (Month > todayMonth || (Month == todayMonth && Day > todayDay ))
+            {
+                age--;
+            }
+
+            output = Name + " is " + age.ToString() + " years old.";
+            return output;
+        }
+    }
+}
